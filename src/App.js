808d553f26cbfad0969d76a2b9f0de14645a3b89
unknown <nandhirajan2013@gmail.com>
2019-10-21 21:09:54 +0530

    Adding comments

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 function App() {
   const JSX = <div>
-    <h1>Test header</h1>
+    {/*Adding Comment*/}
+    < h1 > Test header</h1>
     <p>Test Paragrap</p>
     <ul>
       <li>Line1</li>
       <li>Line2</li>
       <li>Line3</li>
     </ul>
-  </div>
+  </div >
   return JSX;
 }
 
