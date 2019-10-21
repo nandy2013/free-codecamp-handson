791c7aa3a36e7a470063d92f1751ace78a51c64b
unknown <nandhirajan2013@gmail.com>
2019-10-21 21:01:49 +0530

    Compelx JSX enclosed with single parent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <p>hello</p>
-    </div>
-  );
+  const JSX = <div>
+    <h1>Test header</h1>
+    <p>Test Paragrap</p>
+    <ul>
+      <li>Line1</li>
+      <li>Line2</li>
+      <li>Line3</li>
+    </ul>
+  </div>
+  return JSX;
 }
 
 export default App;
